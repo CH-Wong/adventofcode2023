206e2a6bc51abb74437196ea0cb34d6752c855ec
CH-Wong <w.chunheung@gmail.com>
2024-10-31 00:41:12 +0100

    IT WORKS

--- day5-2.py
+++ day5-2.py
@@ -16,21 +16,35 @@ for chunk in chunks[1:]:
 _, seed_ranges = chunks[0].split(": ")
 seed_ranges = [int(val) for val in seed_ranges.split()]
 
-seeds = []
+largest_seed = []
 for s, r in zip(seed_ranges[::2], seed_ranges[1::2]):
-    print(s,r)
-    # seeds.extend(range(s,s+r))
-print(len(seeds))
+    largest_seed.append(s+r)
+largest_seed = max(largest_seed)
 
+locations = []
 
-# locations = []
-# for seed in seeds:
-#     for mappings in mappings_list:
-#         for target, source, steps in mappings:
-#             if source <= seed <= source + steps:
-#                 seed += target - source
-#                 break
+import time
+## invert the look-up, start by results instead, and then look if the seed is contained within the given seed ranges
+mappings_list.reverse()
+start = time.time()
+for result in range (0,largest_seed):
+    seed = result
+    for mappings in mappings_list:
+        for target, source, steps in mappings:
+            if target <= seed <= target + steps:
+                seed += - (target - source)
+                break
 
-#     locations.append(seed)
+    in_ranges = False
+    for s, r in zip(seed_ranges[::2], seed_ranges[1::2]):
+        if s <= seed <= s + r:
+            in_ranges = True
+            print("HELLOO")
+            break
+    if in_ranges == True:
+        break
+    
+    if result%1000000 == 0:
+        print(f"{result}/{largest_seed} = {100*result/largest_seed:.1f}%, {time.time() - start:.2f}s")
 
-# print(min(locations))
+print(seed, result)
